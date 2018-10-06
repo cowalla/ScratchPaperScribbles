f3e7f4c4afbe29bc081314b088e09ccf4f9d1de5
Christopher Robison <cbobrobison@gmail.com>
2018-10-06 16:52:16 -0700

    modified paint pixels

--- contracts/Harbeger.sol
+++ contracts/Harbeger.sol
@@ -8,9 +8,10 @@ contract Harberger{
 	address commons;
 	uint256 timeLimit; // number of blocks auction will stay last before resetting to 0;
 	uint256 minBid;
+	uint256 maxPixels;
 
 	struct ID {
-		string color;
+		hex color;
 		uint256 value;
 		uint256 lastUpdate;
 	};
@@ -24,40 +25,43 @@ contract Harberger{
         _;
     };
 
-	function paintPixel(uint[2] _id, hex _color, uint256 _value) returns(uint256 cost){
-        require(_value > minBid);
+	function paintPixel(uint256 _id, hex _color, uint256 _bid){
         bool expired = (block.number - pixels[_id].lastUpdate) >= timeLimit;
         uint256 cost;
 
-        if(expired == false && pixels[_id].value < _value){
-        	cost = _value;
-        	pixels[_id].value = cost;
+        if(expired == false){
+        	cost = pixels[_id].value;
         }
         else {
-        	cost = minBid;
-        	pixels[_id].value = cost;
-        };
+        	cost = minBid
+        }
 
+        require (cost < _bid)
+
+       	pixels[_id].value = _bid;
         pixels[_id].color = _color;
         pixels[_id].lastUpdate = block.number;
 
-        emit Paint(_id, _color, cost);
+        emit Paint(_id, _color, _bid);
 	};
 
-	function paintPixels(uint256[2][] _ids, hex[] _colors, uint256[] _value) { //add modifier all lengths are the same // add modifier sum[_value] s == msg.send
-		uint256 totalValue;
+	function paintPixels(uint256[] _ids, hex[] _colors, uint256[] _bids) {
+		require(_ids.length == _colors.length == _bids.length);
+		commons.send(msg.value);
+		uint256 paintCredit = msg.value;
 
 		for (uint256 i = 0; i < _ids.length; i++) {
         	_id = _ids[i];
         	_color = _colors[i];
-            _value = _values[i];
-
-            paintPixel(_id, _color, _value) return(cost);
-            totalValue += cost;
+            _bid = _bids[i];
+            paintCredit -= _bid;
+            if (paintCredit >= 0){
+            	paintPixel(_id, _color, _bid);
+            }
+            else{
+            	break;
+            }
         }
-
-        commons.send(totalValue);
-        msg.sender.send(msg.value - totalValue);
 	};
 
 	function changeTimeLimit (uint256 _timeLimit) is onlyOwner {
@@ -74,5 +78,9 @@ contract Harberger{
 
 	function changeOwner (address _owner) is onlyOwner {
 		owner = _owner;
+	};
+
+	function changeMaxPixels (uint256 _maxPixels) is onlyOwner {
+		maxPixels = _maxPixels;
 	};
 }
