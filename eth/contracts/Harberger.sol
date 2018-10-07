40740ce0161f71884daeeb9fafe3208774ee1d8a
Ryan Robison <rojorobison@gmail.com>
2018-10-06 18:52:49 -0700

    Formatted contract

--- eth/contracts/Harberger.sol
+++ eth/contracts/Harberger.sol
@@ -1,95 +1,95 @@
 pragma solidity ^0.4.23;
 pragma experimental ABIEncoderV2;
 
-contract Harberger{
+contract Harberger {
 
-	constructor() public { 
-		owner = msg.sender; 
-	}
+    constructor() public {
+        owner = msg.sender;
+    }
     address owner;
 
-	address commons;
-	uint256 timeLimit; // number of blocks auction will stay last before resetting to 0;
-	uint256 minBid;
-	uint256 maxPixels;
+    address commons;
+    uint256 timeLimit; // number of blocks auction will stay last before resetting to 0;
+    uint256 minBid;
+    uint256 maxPixels;
 
-	struct ID {
-		string color;
-		uint256 value;
-		uint256 lastUpdate;
-	}
+    struct ID {
+        string color;
+        uint256 value;
+        uint256 lastUpdate;
+    }
 
-	mapping (uint => ID) pixels;
+    mapping(uint => ID) pixels;
 
-	event Paint(uint ID, string Color, uint256 Value);
+    event Paint(uint ID, string Color, uint256 Value);
 
-	modifier onlyOwner {
+    modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
 
     function paintPixel(uint256 _id, string _color, uint256 _bid) payable public {
-    	require (_bid == msg.value);
-    	commons.transfer(msg.value);
+        require(_bid == msg.value);
+        commons.transfer(msg.value);
         paintPixel(_id, _color, _bid);
-	}
+    }
 
-	function singlePixel(uint256 _id, string _color, uint256 _bid) internal {
+    function singlePixel(uint256 _id, string _color, uint256 _bid) internal {
         bool expired = (block.number - pixels[_id].lastUpdate) >= timeLimit;
         uint256 cost;
 
-        if(expired == false){
-        	cost = pixels[_id].value;
+        if (expired == false) {
+            cost = pixels[_id].value;
         }
         else {
-        	cost = minBid;
+            cost = minBid;
         }
 
-        require (cost < _bid);
+        require(cost < _bid);
 
-       	pixels[_id].value = _bid;
+        pixels[_id].value = _bid;
         pixels[_id].color = _color;
         pixels[_id].lastUpdate = block.number;
 
         emit Paint(_id, _color, _bid);
-	}
+    }
 
-	function paintPixels(uint256[] _ids, string[] _colors, uint256[] _bids) payable public {
-		//require(_ids.length == _colors.length == _bids.length);
-		commons.transfer(msg.value);
-		uint256 paintCredit = msg.value;
+    function paintPixels(uint256[] _ids, string[] _colors, uint256[] _bids) payable public {
+        //require(_ids.length == _colors.length == _bids.length);
+        commons.transfer(msg.value);
+        uint256 paintCredit = msg.value;
 
-		for (uint256 i = 0; i < _ids.length; i++) {
-        	uint256 _id = _ids[i];
-        	string memory _color = _colors[i];
+        for (uint256 i = 0; i < _ids.length; i++) {
+            uint256 _id = _ids[i];
+            string memory _color = _colors[i];
             uint256 _bid = _bids[i];
             paintCredit -= _bid;
-            if (paintCredit >= 0){
-            	paintPixel(_id, _color, _bid);
+            if (paintCredit >= 0) {
+                paintPixel(_id, _color, _bid);
             }
-            else{
-            	break;
+            else {
+                break;
             }
         }
-	}
+    }
 
-	function changeTimeLimit (uint256 _timeLimit) public onlyOwner {
-		timeLimit = _timeLimit;
-	}
+    function changeTimeLimit(uint256 _timeLimit) public onlyOwner {
+        timeLimit = _timeLimit;
+    }
 
-	function changeMinBid (uint256 _minBid) public onlyOwner {
-		minBid = _minBid;
-	}
+    function changeMinBid(uint256 _minBid) public onlyOwner {
+        minBid = _minBid;
+    }
 
-	function changeCommons (address _commons) public onlyOwner {
-		commons = _commons;
-	}
+    function changeCommons(address _commons) public onlyOwner {
+        commons = _commons;
+    }
 
-	function changeOwner (address _owner) public onlyOwner {
-		owner = _owner;
-	}
+    function changeOwner(address _owner) public onlyOwner {
+        owner = _owner;
+    }
 
-	function changeMaxPixels (uint256 _maxPixels) public onlyOwner {
-		maxPixels = _maxPixels;
-	}
+    function changeMaxPixels(uint256 _maxPixels) public onlyOwner {
+        maxPixels = _maxPixels;
+    }
 }
